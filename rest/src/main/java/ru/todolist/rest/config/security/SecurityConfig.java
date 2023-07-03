1c46f60cfc5473f7b755c9a1bfa3f8fd87fea95f
osiptsoff <osipczovnikita@gmail.com>
2023-07-03 17:52:39 +0300

    added cors

--- rest/src/main/java/ru/todolist/rest/config/security/SecurityConfig.java
+++ rest/src/main/java/ru/todolist/rest/config/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package ru.todolist.rest.config.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import ru.todolist.rest.security.JwtFilter;
 
@@ -39,5 +44,15 @@ public class SecurityConfig {
         		.build();
 
     }
+    
+    @Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/rest/**", configuration);
+		return source;
+	}
 	
 }
